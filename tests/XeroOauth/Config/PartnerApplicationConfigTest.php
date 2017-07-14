d2ba291c24034cdc3de3d308286c14d4013ab65b
Mikhail Levanov <leor.thesweetvoice@gmail.com>
2017-07-14 07:33:54 +0400

    PartnerConfig test and fix

---
+++ tests/XeroOauth/Config/PartnerApplicationConfigTest.php
@@ -0,0 +1,73 @@
+<?php
+namespace DarrynTen\XeroOauth\Tests\XeroOauth\Config;
+
+use DarrynTen\XeroOauth\Config\PartnerApplicationConfig;
+use DarrynTen\XeroOauth\Exception\ConfigException;
+use DarrynTen\XeroOauth\Exception\ExceptionMessages;
+
+class PartnerApplicationConfigTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test key value
+     */
+    const TEST_KEY = 'testKey';
+
+    /**
+     * Test dummy endpoint value
+     */
+    const TEST_ENDPOINT = 'http://localhost:8082';
+
+    /**
+     * @var PartnerApplicationConfig
+     */
+    private $configObject;
+
+    /**
+     * Creates an instance of a test object
+     */
+    public function setUp()
+    {
+        $this->configObject = new PartnerApplicationConfig([
+            'key' => static::TEST_KEY,
+            'endpoint' => static::TEST_ENDPOINT
+        ]);
+    }
+
+    /**
+     * Checks that we have an instance of right class
+     */
+    public function testInstanceOf()
+    {
+        $this->assertInstanceOf(
+            PartnerApplicationConfig::class,
+            $this->configObject,
+            sprintf('Config must be an instance of %s', PartnerApplicationConfig::class)
+        );
+    }
+
+    /**
+     * Checks that getRequestHandlerConfig method returns right values
+     */
+    public function testGetRequestHandlerConfig()
+    {
+        $handlerConfig = $this->configObject->getRequestHandlerConfig();
+
+        $this->assertTrue(is_array($handlerConfig), 'Config is not an array');
+        $this->assertArrayHasKey('key', $handlerConfig, 'Config does not contain key `key`');
+        $this->assertEquals(static::TEST_KEY, $handlerConfig['key'], 'Key is wrong');
+        $this->assertArrayHasKey('endpoint', $handlerConfig, 'Config does not contain key `endpoint`');
+        $this->assertEquals(static::TEST_ENDPOINT, $handlerConfig['endpoint'], 'Endpoint is wrong');
+    }
+
+    /**
+     * Checks that constructor init methods throws Exception
+     */
+    public function testConstructorException()
+    {
+        $this->expectException(ConfigException::class);
+        $this->expectExceptionCode(ConfigException::MISSING_KEY);
+        $this->expectExceptionMessage(ExceptionMessages::$configErrorMessages[ConfigException::MISSING_KEY]);
+
+        $configObject = new PartnerApplicationConfig([ ]);
+    }
+}
