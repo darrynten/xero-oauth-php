113c78dd83a3b120d87bd71e55b95ef15489e105
Mikhail Levanov <leor.thesweetvoice@gmail.com>
2017-07-14 14:05:23 +0400

    Tests for a ConfigException class

---
+++ tests/XeroOauth/Exception/ApiExceptionTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jguru
+ * Date: 14.07.17
+ * Time: 14:06
+ */
+
+namespace DarrynTen\XeroOauth\Exception;
+
+
+class ApiExceptionTest extends \PHPUnit_Framework_TestCase
+{
+
+}
